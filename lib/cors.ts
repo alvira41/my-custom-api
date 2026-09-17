88175b5eff4ebc0b9eab58218d8cda9c8372f60d
alvira41 <alviraoct08@gmail.com>
2026-09-17 14:42:10 +0700

    fix cors

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
 
-const frontendUrl =
-  process.env.FRONTEND_URL || "http://localhost:3000";
-
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": frontendUrl,
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods":
     "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers":
@@ -12,11 +9,9 @@ export const corsHeaders = {
 };
 
 export function withCors(response: NextResponse) {
-  Object.entries(corsHeaders).forEach(
-    ([key, value]) => {
-      response.headers.set(key, value);
-    }
-  );
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
 
   return response;
 }
