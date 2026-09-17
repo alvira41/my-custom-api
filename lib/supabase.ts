83257753257bf4b0581d551f771d9f772b12810d
alvira41 <alviraoct08@gmail.com>
2026-09-17 12:33:58 +0700

    fix supabase client

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,23 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!supabaseUrl || !supabaseServiceKey) {
-  throw new Error('Supabase URL atau Service Role Key belum terpasang di .env.local!');
+if (!supabaseUrl) {
+  throw new Error("SUPABASE_URL belum terpasang.");
 }
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
+if (!supabaseServiceKey) {
+  throw new Error("SUPABASE_SERVICE_ROLE_KEY belum terpasang.");
+}
+
+export const supabaseAdmin = createClient(
+  supabaseUrl,
+  supabaseServiceKey,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+    },
+  }
+);
